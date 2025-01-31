9126807cd1c6f24bd41e0b84d2f9d76164bf8a5e
ishanrana363 <ishanrana363@gmail.com>
2025-01-31 11:59:34 +0600

    task update api created

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -6,7 +6,7 @@ const createTask = async (req, res) => {
         const { id } = req.headers;
         const { title, description } = req.body;
 
-        
+
 
         const newTask = new todoModel({ userId: id, title, description });
         const savedTask = await newTask.save();
@@ -17,20 +17,44 @@ const createTask = async (req, res) => {
     }
 };
 
-const taskByUser = async (req,res)=>{
+const taskByUser = async (req, res) => {
     try {
         const id = req.headers.id;
         const filter = {
-            userId :id,
+            userId: id,
         };
-        const tasks = await todoModel.find(filter).sort({createdAt:-1});
-        if(tasks.length===0){
-            return errorResponse(res, 404,'No tasks found',null);
+        const tasks = await todoModel.find(filter).sort({ createdAt: -1 });
+        if (tasks.length === 0) {
+            return errorResponse(res, 404, 'No tasks found', null);
         }
         return successResponse(res, 200, "Tasks retrieved successfully", tasks);
     } catch (error) {
-        return errorResponse(res,500,"Something went wrong",error);
+        return errorResponse(res, 500, "Something went wrong", error);
     }
-}
+};
 
-module.exports = { createTask,taskByUser };
+const updateTask = async (req, res) => {
+    try {
+        const id = req.headers.id;
+        const taskId = req.params.id;
+        const title = req.body.title;
+        const description = req.body.description;
+        const updateData = {
+            title,
+            description,
+        }
+        const filter = {
+            _id: taskId,
+            userId: id,
+        };
+        const updatedTask = await todoModel.findOneAndUpdate(filter, updateData, { new: true });
+        if (!updatedTask) {
+            return errorResponse(res, 404, "Task not found", null);
+        }
+        return successResponse(res, 200, "Task updated successfully", updatedTask);
+    } catch (error) {
+        return errorResponse(res,500,"Something went wrong", error);
+    }
+};
+
+module.exports = { createTask, taskByUser,updateTask };
--- src/route/api.js
+++ src/route/api.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const { registration, login, userProfile, updateUserProfile } = require('../controller/userController');
 const authMiddleware = require('../middleware/authMiddleware');
-const { createTask, taskByUser } = require('../controller/taskController');
+const { createTask, taskByUser, updateTask } = require('../controller/taskController');
 
 const router = express.Router();
 
 // user related api
 router.post("/register", registration);
-router.post("/login", login );
-router.get("/profile", authMiddleware,userProfile );
-router.put("/update-profile", authMiddleware, updateUserProfile );
+router.post("/login", login);
+router.get("/profile", authMiddleware, userProfile);
+router.put("/update-profile", authMiddleware, updateUserProfile);
 
 // task related api
 
-router.post("/tasks", authMiddleware, createTask );
-router.get("/task-by-user", authMiddleware, taskByUser );
+router.post("/tasks", authMiddleware, createTask);
+router.get("/task-by-user", authMiddleware, taskByUser);
+router.put("/task-update/:id", authMiddleware, updateTask);
 
 
 module.exports = router;
