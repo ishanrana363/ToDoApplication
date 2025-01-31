359b768256f38d64630c0773cdf0956264890d79
ishanrana363 <ishanrana363@gmail.com>
2025-01-31 09:43:42 +0600

    todo model completed

---
+++ src/model/todoModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+
+const { Schema, model } = mongoose;
+
+
+
+const todoSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'userModel'
+    },
+    title: {
+        type: String,
+        trim: true,
+        minlength: 5,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        trim: true,
+        minlength: 10,
+        maxlength: 500
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Completed', 'In Progress'],
+        default: 'Pending'
+    }
+}, { timestamps: true, versionKey: false });
+
+const todoModel = model("todos", todoSchema);
+
+
+module.exports = todoModel;
